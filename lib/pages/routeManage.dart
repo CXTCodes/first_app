730a4c36c490d4993f1472844b6b251c77f7bbfe
Administrator <RuitongAndroid@126.com>
2021-08-11 17:23:04 +0800

    进度2.2.1——1

--- lib/pages/routeManage.dart
+++ lib/pages/routeManage.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RouteManage extends StatelessWidget {
@@ -18,25 +17,40 @@ class RouteManage extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: ListView(
           children: [
+
+            //TODO 一个简单的示例
             MaterialButton(
                 child: Text("一个简单的示例"),
                 textColor: Colors.white,
                 color: Colors.blue,
                 onPressed: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Demo();
+                    return Demo(title: "新路由", text: "这是一个新路由");
                   }));
                 }),
 
+            //TODO 路由传值
             MaterialButton(
-                child: Text("一个简单的示例"),
+                child: Text("路由传值"),
                 textColor: Colors.white,
                 color: Colors.blue,
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Demo();
+                onPressed: () async {
+
+                  var backValue = await Navigator.push(context,
+                      MaterialPageRoute(builder: (context) {
+                    return TipRoute(text: "text1");
                   }));
+
+                  print(backValue);
                 }),
+
+            //TODO 命名路由
+            MaterialButton(
+                child: Text("命名路由"),
+                textColor: Colors.white,
+                color: Colors.blue,
+                onPressed: () {})
+
           ],
         ),
       ),
@@ -46,19 +60,60 @@ class RouteManage extends StatelessWidget {
   }
 }
 
-//MaterialPageRoute
+//TODO 路由传值
+class TipRoute extends StatelessWidget {
+  final String text;
 
-//一个简单示例
-class Demo extends StatelessWidget {
+  TipRoute({
+    Key? key,
+    required this.text, // 接收一个text参数
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     Scaffold scaffold = new Scaffold(
       appBar: AppBar(
-        title: Text("新路由"),
+        title: Text("提示"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: [
+              Text(text),
+              MaterialButton(
+                  child: Text("返回"),
+                  color: Colors.grey[350],
+                  onPressed: () {
+                    Navigator.pop(context, "返回值");
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+
+    return scaffold;
+  }
+}
+
+//TODO 一个简单示例
+class Demo extends StatelessWidget {
+  String title;
+  String text;
+
+  Demo({required this.title, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    Scaffold scaffold = new Scaffold(
+      appBar: AppBar(
+        title: Text(title),
         backgroundColor: Colors.blue,
       ),
       body: Center(
-        child: Text("这是一个新路由"),
+        child: Text(text),
       ),
     );
 
